6251b9178ecc69b9e02a409a4d4a4bef7fc6b0bf
Nabeel <87926260+Mohammed-Nabeel-K@users.noreply.github.com>
2024-11-06 19:42:03 +0530

    Time: 704 ms (91.62%), Space: 0B (100%) - LeetHub

--- 1882-the-number-of-employees-which-report-to-each-employee/1882-the-number-of-employees-which-report-to-each-employee.sql
+++ 1882-the-number-of-employees-which-report-to-each-employee/1882-the-number-of-employees-which-report-to-each-employee.sql
@@ -1,4 +1,4 @@
 /* Write your T-SQL query statement below */
 WITH nnn AS (SELECT reports_to , COUNT(name) AS reports_count,ROUND(AVG(CAST(age AS DECIMAL(10,2))),0) AS average_age FROM Employees GROUP BY reports_to HAVING reports_to IS NOT NULL)
 
-SELECT DISTINCT employee_id,name,reports_count,average_age FROM Employees e LEFT JOIN nnn ON e.employee_id = nnn.reports_to WHERE reports_count IS NOT NULL
+SELECT DISTINCT employee_id,name,reports_count,average_age FROM Employees e JOIN nnn ON e.employee_id = nnn.reports_to 
