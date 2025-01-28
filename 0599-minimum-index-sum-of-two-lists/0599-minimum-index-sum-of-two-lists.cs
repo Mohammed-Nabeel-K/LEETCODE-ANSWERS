10fd433d463bb6b0f12342b6dfee689e93a13148
Nabeel <87926260+Mohammed-Nabeel-K@users.noreply.github.com>
2025-01-28 12:00:30 +0530

    Time: 100 ms (5.4%), Space: 62 MB (50.68%) - LeetHub

--- 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.cs
+++ 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.cs
@@ -1,19 +1,17 @@
 public class Solution {
     public string[] FindRestaurant(string[] list1, string[] list2) {
         Dictionary<string,int> dict = new Dictionary<string,int>();
+        int min = int.MaxValue;
         foreach(string i in list1){
             if(list2.Contains(i)){
                 int idx = Array.IndexOf(list1,i)+ Array.IndexOf(list2,i);
                 dict.Add(i,idx);
-                
+                if(min>idx){
+                    min = idx;
+                }
             }
         }
-        int min = int.MaxValue;
-        foreach(int value in dict.Values){
-            if(min>value){
-                min = value;
-            }
-        }
+        
         List<string> arr = new List<string>();
         foreach(var item in dict){
             if(item.Value == min){
