8439f54cb571580a691492fd732374caf3bf13df
Nabeel <87926260+Mohammed-Nabeel-K@users.noreply.github.com>
2025-01-28 12:03:19 +0530

    Time: 114 ms (5.4%), Space: 62.1 MB (44.59%) - LeetHub

--- 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.cs
+++ 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.cs
@@ -1,23 +1,32 @@
 public class Solution {
     public string[] FindRestaurant(string[] list1, string[] list2) {
-        Dictionary<string,int> dict = new Dictionary<string,int>();
+        Dictionary<string, int> dict = new Dictionary<string, int>();
         int min = int.MaxValue;
-        foreach(string i in list1){
-            if(list2.Contains(i)){
-                int idx = Array.IndexOf(list1,i)+ Array.IndexOf(list2,i);
-                dict.Add(i,idx);
-                if(min>idx){
+
+        // Store the indices of common restaurants from both lists
+        foreach (string i in list1) {
+            if (list2.Contains(i)) {
+                int idx = Array.IndexOf(list1, i) + Array.IndexOf(list2, i);
+                
+                // If the restaurant already exists, we update the value (no need to use Add)
+                dict[i] = idx;  // This works even if the restaurant already exists in the dictionary
+
+                // Update the minimum sum of indices
+                if (min > idx) {
                     min = idx;
                 }
             }
         }
         
         List<string> arr = new List<string>();
-        foreach(var item in dict){
-            if(item.Value == min){
+        // Collect all restaurants with the minimum index sum
+        foreach (var item in dict) {
+            if (item.Value == min) {
                 arr.Add(item.Key);
             }
         }
+
+        // Return the result as an array
         return arr.ToArray();
     }
 }
